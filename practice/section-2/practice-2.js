50ee25e7a0e535e2f5e6bcf2a4549ee21efb62b6
huyuanixn325 <huyuanxin0325@163.com>
2019-08-19 22:38:35 +0800

    finish the test

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,24 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result = [];
+  var map = new Map();
+  //alert(collection[collection.length-1]);
+  for (var i=0;i<collection.length-1;i++){
+    if (map.get(collection[i])==null){
+      map.set(collection[i],1);
+    }else{
+      map.set(collection[i],map.get(collection[i])+1);
+    }
+  }
+
+  var temp = collection[collection.length-1].split("-");
+  //console.log(temp);
+  map.set(temp[0],Number(temp[1]));
+  //console.log(map);
+  map.forEach(function(key,value){
+    result.push({key:value,count:key})
+  });
+  console.log(result);
+  return result;
 }
--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,53 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result = [];
+  var map = new Map();
+  var temp=null;
+  //alert(collection[collection.length-1].length);
+  for (var i=0;i<collection.length;i++){
+    console.log(collection[i].length>1);
+    if (collection[i].length>1){
+      temp=collection[i].substring(0,1);
+    }else temp=collection[i];
+
+    if (map.get(temp)==null){
+
+      if (collection[i].length>1&&collection[i].length<=3) {
+        console.log(temp+"----"+collection[i].substring(2, collection[i].length));
+        //  console.log(collection[i].substring(2,collection[i].length-1));
+        map.set(collection[i].substring(0,1), Number(collection[i].substring(2, collection[i].length)))
+        console.log(map.get(collection[i].substring(0,1)))
+        //  console.log(collection[i].substring(1,collection[i].length-1));
+      }else if(collection[i].length>3){
+        map.set(collection[i].substring(0,1), Number(collection[i].substring(2, collection[i].length-1)))
+      }
+      else{
+        map.set(temp,1);
+      }
+    }else{
+        if (collection[i].length>1&&collection[i].length<=3) {
+        //  console.log(collection[i].substring(2,collection[i].length-1));
+        map.set(collection[i].substring(0,1),map.get(collection[i].substring(0,1))+Number(collection[i].substring(2,collection[i].length)))
+        //  console.log(collection[i].substring(1,collection[i].length-1));
+      }else if (collection[i].length>3) {
+          //  console.log(collection[i].substring(2,collection[i].length-1));
+          map.set(collection[i].substring(0,1),map.get(collection[i].substring(0,1))+Number(collection[i].substring(2,collection[i].length-1)))
+          //  console.log(collection[i].substring(1,collection[i].length-1));
+        }
+      else{
+          map.set(collection[i],map.get(collection[i])+1);
+     }
+
+    }
+  }
+
+
+  //console.log(temp);
+  console.log(map);
+  map.forEach(function(key,value){
+    result.push({name:value,summary:key})
+  });
+  console.log(result);
+  return result;
 }
